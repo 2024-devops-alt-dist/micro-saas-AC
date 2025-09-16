d8d1c55f7b8a5cd7c044df13bf6037ae546122fc
Agnès Cappello <senga.ds@gmail.com>
2025-09-16 22:32:36 +0200

    chore(project): initialize Django backend and add Tailwind CSS to React front

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from '../components/Button.tsx'
+
+function Home() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">QUIZPILOT</h1>
+      <p className="mt-2 text-lg">Fais décoller tes révisions</p>
+      <Button />
+    </div>
+  )
+}
+
+export default Home
