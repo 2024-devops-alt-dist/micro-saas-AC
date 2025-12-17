9f3a1eab9c0d898fc58de38e82c6903486572274
Agnès Cappello <senga.ds@gmail.com>
2025-12-17 22:32:15 +0100

    feat: Add client Dockerfile, define Django models for existing Category and Level tables, and remove PostgreSQL service from production compose.

--- back-django/api/models.py
+++ back-django/api/models.py
@@ -1,20 +1,48 @@
-import uuid
+# import uuid
+
+# from django.contrib.auth.models import User
+# from django.db import models
+
+
+# class Category(models.Model):
+#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+#     name = models.CharField(max_length=100)
+
+#     def __str__(self):
+#         return f"Category: {self.name}"
+
+
+# class Level(models.Model):
+#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+#     name = models.CharField(max_length=100)
+
+#     def __str__(self):
+#         return f"Level: {self.name}"
+
 
 from django.contrib.auth.models import User
 from django.db import models
 
 
 class Category(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # L'id est automatiquement un IntegerField (comme sur n8n)
     name = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "category"  # On pointe vers la table existante
+        managed = False  # On interdit à Django de modifier la structure
 
     def __str__(self):
         return f"Category: {self.name}"
 
 
 class Level(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # L'id est automatiquement un IntegerField
     name = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "level"  # On pointe vers la table existante
+        managed = False
 
     def __str__(self):
         return f"Level: {self.name}"
