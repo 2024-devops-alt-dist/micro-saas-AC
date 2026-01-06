2b7f11e03b48ebc9a97febed9d0580af9b990523
Agnès Cappello <senga.ds@gmail.com>
2026-01-06 20:54:53 +0100

    feat: add tables + urls

--- back-django/api/views.py
+++ back-django/api/views.py
@@ -2,8 +2,14 @@ from django.contrib.auth.models import User
 from django.http import JsonResponse
 from rest_framework import generics
 
-from .models import Category, Level
-from .serializers import CategorySerializer, LevelSerializer, UserSerializer
+from .models import Category, Level, Questions, Propositions
+from .serializers import (
+    CategorySerializer,
+    LevelSerializer,
+    UserSerializer,
+    QuestionsSerializer,
+    PropositionsSerializer,
+)
 
 
 def test_api(request):
@@ -43,4 +49,26 @@ class UserListCreateView(generics.ListCreateAPIView):
 class UserRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    lookup_field = "id"
+
+
+class QuestionsListCreateView(generics.ListCreateAPIView):
+    queryset = Questions.objects.all()
+    serializer_class = QuestionsSerializer
+
+
+class QuestionsRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Questions.objects.all()
+    serializer_class = QuestionsSerializer
+    lookup_field = "id"
+
+
+class PropositionsListCreateView(generics.ListCreateAPIView):
+    queryset = Propositions.objects.all()
+    serializer_class = PropositionsSerializer
+
+
+class PropositionsRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Propositions.objects.all()
+    serializer_class = PropositionsSerializer
     lookup_field = "id"
