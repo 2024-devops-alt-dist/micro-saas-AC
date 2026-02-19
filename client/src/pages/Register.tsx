a6fd160449a284bd195a044184a134e7b6964bd2
Agnès Cappello <senga.ds@gmail.com>
2026-02-19 19:04:31 +0100

    feat: add Register title

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,9 +1,11 @@
 import AuthForm from "../features/auth/components/AuthForm";
 import BottomNav from "../components/BottomNav";
+import Title from "../components/Title";
 
 export default function Register() {
     return (
         <div className="min-h-screen bg-gray-900 pb-20">
+            <Title text="CONNEXION" />
             <AuthForm initialIsLogin={false} />
             <BottomNav />
         </div>
